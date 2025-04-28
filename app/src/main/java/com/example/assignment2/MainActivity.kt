820467fa39d4da3a14bc56cb997ee5192aea6033
Bandile <ST10480829@vcconnect.edu.ze>
2025-04-28 09:48:59 +0200

    Components Called (activity_main.xml to MainActivity.kt)

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.assignment2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //==========================================================================================
+        //Calling all textview and button components from the front end code of the main activity.
+        //Calling components allows the program to know what the components being used are and where the components being used are found.
+
+        val txtTo = findViewById<TextView>(R.id.txt_To)
+        val txtWelcome = findViewById<TextView>(R.id.txt_Welcome)
+        val txtHistory = findViewById<TextView>(R.id.txt_History)
+        val txtQuestion = findViewById<TextView>(R.id.txt_Question)
+        val btnContinue = findViewById<Button>(R.id.btn_Continue)
+
+        //==========================================================================================
+
+       
+
     }
 }
