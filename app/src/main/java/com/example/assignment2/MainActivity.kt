48722cc033f9d3a81aefdffe01af9db1ed6cbae0
Bandile <ST10480829@vcconnect.edu.ze>
2025-04-29 09:34:29 +0200

    Flashcard Question screen(Created, No code)

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.assignment2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -24,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         val btnContinue = findViewById<Button>(R.id.btn_Continue)
 
         //==========================================================================================
+
+
+
 
        
 
