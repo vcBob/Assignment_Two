9bbd8e648132eb22fc29f20012505d544745ad26
Bandile <ST10480829@vcconnect.edu.ze>
2025-05-01 19:41:57 +0200

    Flashcard Question screen(Component names called to MainActivity2.kt)

--- app/src/main/java/com/example/assignment2/MainActivity2.kt
+++ app/src/main/java/com/example/assignment2/MainActivity2.kt
@@ -1,6 +1,8 @@
 package com.example.assignment2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,15 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+        //==========================================================================================
+        // The code below calls all the id names of the components from the activity_main2.xml folder over to the MainActivity2.kt
+        val txtQuestion2 = findViewById<TextView>(R.id.txt_Question2)
+        val txtHistQuestion = findViewById<TextView>(R.id.txt_HistQuestion2)
+        val txtFeedback = findViewById<TextView>(R.id.txt_Feedback)
+        val btnTrue = findViewById<Button>(R.id.btn_True)
+        val btnFalse = findViewById<TextView>(R.id.btn_False)
+        val btnNext = findViewById<TextView>(R.id.btn_Next)
+        //==========================================================================================
+
     }
 }
